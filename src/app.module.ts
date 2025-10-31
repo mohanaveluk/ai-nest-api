82c126be3d0e74333401316383bce7b5164008a8
Mohanavelu <mohanaveluk@gmail.com>
2025-10-30 22:50:14 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      //load: [databaseConfig, jwtConfig, adminConfig, googleCloudConfig, smtpConfig],
+      isGlobal: true,
+    }),    
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
